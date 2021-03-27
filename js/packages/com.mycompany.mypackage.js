5915db8fa77a3a6bb8daafadcc036cdc077da873
Jean Machuca <jeanmachuca@users.noreply.github.com>
2021-03-27 17:36:14 -0300

    Update com.mycompany.mypackage.js

--- js/packages/com.mycompany.mypackage.js
+++ js/packages/com.mycompany.mypackage.js
@@ -7,7 +7,9 @@ Package('com.mycompany.mypackage',[
             var spaceContext = New (MySpace2d, {});
 
             // by setting an interval to draw we got a space traveling effect as a result
-            setInterval (spaceContext.draw);
+            setInterval (function (){
+                spaceContext.draw();
+            });
 
              // this will append the canvas element to the component
             this.body.append (spaceContext);
